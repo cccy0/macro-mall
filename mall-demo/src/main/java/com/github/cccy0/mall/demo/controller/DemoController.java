a86a90a60fc2a9ff1ea1b1204eae239ba350780f
cccy <cccy0@foxmail.com>
2020-09-21 23:06:31 +0800

    :bug: logback.xml -> logback-spring.xml | CURD Demo

--- mall-demo/src/main/java/com/github/cccy0/mall/demo/controller/DemoController.java
+++ mall-demo/src/main/java/com/github/cccy0/mall/demo/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.github.cccy0.mall.demo.controller;
 
 import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.demo.dto.PmsBrandDto;
 import com.github.cccy0.mall.demo.service.DemoService;
 import com.github.cccy0.mall.model.PmsBrand;
 import io.swagger.annotations.Api;
@@ -8,9 +9,8 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +37,47 @@ public class DemoController {
         return CommonResult.success(demoService.listAllBrand());
     }
 
-    // todo: test query success, need test other
+    @ApiOperation(value = "添加品牌")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Object> createBrand(@Validated @RequestBody PmsBrandDto pmsBrand) {
+        CommonResult<Object> commonResult;
+        int count = demoService.createBrand(pmsBrand);
+        if (count == 1) {
+            commonResult = CommonResult.success(count);
+            logger.debug("createBrand success:{}", pmsBrand);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            logger.debug("createBrand failed:{}", pmsBrand);
+        }
+        return commonResult;
+    }
+
+    @ApiOperation(value = "更新品牌")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Object> updateBrand(@PathVariable("id") Long id,
+                                            @Validated @RequestBody PmsBrandDto pmsBrand) {
+        CommonResult<Object> commonResult;
+        int count = demoService.updateBrand(id, pmsBrand);
+        if (count == 1) {
+            commonResult = CommonResult.success(count);
+            logger.debug("updateBrand success:{}", pmsBrand);
+        } else {
+            commonResult = CommonResult.failed("操作失败");
+            logger.debug("updateBrand failed:{}", pmsBrand);
+        }
+        return commonResult;
+    }
+
+    @ApiOperation(value = "删除品牌")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public CommonResult<Object> deleteBrand(@PathVariable("id") Long id) {
+        int count = demoService.deleteBrand(id);
+        if (count == 1) {
+            logger.debug("deleteBrand success, id={}", id);
+            return CommonResult.success(null);
+        } else {
+            logger.debug("deleteBrand failed, id={}", id);
+            return CommonResult.failed("操作失败");
+        }
+    }
 }
