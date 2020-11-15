0b47d5d6e5dfef5a524a0c151bd9381bdeafaeac
cccy <cccy0@foxmail.com>
2020-11-16 00:25:37 +0800

    :construction: recommendSubject controller

---
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeRecommendSubjectController.java
@@ -0,0 +1,17 @@
+package com.github.cccy0.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * todo: 首页专题推荐管理Controller
+ *
+ * @author Zhai
+ * 2020/11/16 0:23
+ */
+@RestController
+@Api(tags = "首页专题推荐管理")
+@RequestMapping("/home/recommendSubject")
+public class SmsHomeRecommendSubjectController {
+}
