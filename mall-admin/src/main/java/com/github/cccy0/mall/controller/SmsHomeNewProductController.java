55480bb72d83afe0cb06f2d390fd81d88649a9bf
cccy <cccy0@foxmail.com>
2020-11-19 22:28:51 +0800

    :new: recommendProduct controller

---
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeNewProductController.java
@@ -0,0 +1,18 @@
+package com.github.cccy0.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * todo: 首页新品管理Controller
+ *
+ * @author Zhai
+ * 2020/11/19 22:27
+ */
+
+@RestController
+@Api(tags = "首页新品管理")
+@RequestMapping("/home/newProduct")
+public class SmsHomeNewProductController {
+}
--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeRecommendProductController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeRecommendProductController.java
@@ -1,11 +1,18 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.model.SmsHomeRecommendProduct;
+import com.github.cccy0.mall.service.SmsHomeRecommendProductService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * todo: 首页人气推荐管理Controller
+ * 首页人气推荐管理Controller
  *
  * @author Zhai
  * 2020/11/17 22:15
@@ -14,4 +21,60 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "首页人气推荐管理")
 @RequestMapping("/home/recommendProduct")
 public class SmsHomeRecommendProductController {
+    private SmsHomeRecommendProductService recommendProductService;
+
+    @Autowired
+    public void setRecommendProductService(SmsHomeRecommendProductService recommendProductService) {
+        this.recommendProductService = recommendProductService;
+    }
+
+    @ApiOperation("添加首页推荐")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody List<SmsHomeRecommendProduct> homeBrandList) {
+        int count = recommendProductService.create(homeBrandList);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改推荐排序")
+    @RequestMapping(value = "/update/sort/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> updateSort(@PathVariable Long id, Integer sort) {
+        int count = recommendProductService.updateSort(id, sort);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量删除推荐")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@RequestParam("ids") List<Long> ids) {
+        int count = recommendProductService.delete(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量修改推荐状态")
+    @RequestMapping(value = "/update/recommendStatus", method = RequestMethod.POST)
+    public CommonResult<Integer> updateRecommendStatus(@RequestParam("ids") List<Long> ids, @RequestParam Integer recommendStatus) {
+        int count = recommendProductService.updateRecommendStatus(ids, recommendStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("分页查询推荐")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsHomeRecommendProduct>> list(@RequestParam(value = "productName", required = false) String productName,
+                                                                  @RequestParam(value = "recommendStatus", required = false) Integer recommendStatus,
+                                                                  @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                                  @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsHomeRecommendProduct> homeBrandList = recommendProductService.list(productName, recommendStatus, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(homeBrandList));
+    }
 }
