d459b6eeb7f5d1c347dfc7f6163e7610e825b4e7
cccy <cccy0@foxmail.com>
2020-11-23 00:01:55 +0800

    :new: newProduct controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeNewProductController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeNewProductController.java
@@ -1,13 +1,18 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.model.SmsHomeNewProduct;
 import com.github.cccy0.mall.service.SmsHomeNewProductService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * todo: 首页新品管理Controller
+ * 首页新品管理Controller
  *
  * @author Zhai
  * 2020/11/19 22:27
@@ -22,5 +27,55 @@ public class SmsHomeNewProductController {
     @Autowired
     public void setHomeNewProductService(SmsHomeNewProductService homeNewProductService) {
         this.homeNewProductService = homeNewProductService;
+    }
+
+    @ApiOperation("添加首页推荐品牌")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody List<SmsHomeNewProduct> homeBrandList) {
+        int count = homeNewProductService.create(homeBrandList);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改推荐排序")
+    @RequestMapping(value = "/update/sort/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> updateSort(@PathVariable Long id, Integer sort) {
+        int count = homeNewProductService.updateSort(id, sort);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量删除推荐")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@RequestParam("ids") List<Long> ids) {
+        int count = homeNewProductService.delete(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量修改推荐状态")
+    @RequestMapping(value = "/update/recommendStatus", method = RequestMethod.POST)
+    public CommonResult<Integer> updateRecommendStatus(@RequestParam("ids") List<Long> ids, @RequestParam Integer recommendStatus) {
+        int count = homeNewProductService.updateRecommendStatus(ids, recommendStatus);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("分页查询推荐")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsHomeNewProduct>> list(@RequestParam(value = "productName", required = false) String productName,
+                                                            @RequestParam(value = "recommendStatus", required = false) Integer recommendStatus,
+                                                            @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsHomeNewProduct> homeBrandList = homeNewProductService.list(productName, recommendStatus, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(homeBrandList));
     }
 }
