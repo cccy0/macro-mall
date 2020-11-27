8cfe9b9c65cd466bad75d61fef0c1e28330a227e
cccy <cccy0@foxmail.com>
2020-11-27 23:05:41 +0800

    :new: advertise controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeAdvertiseController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsHomeAdvertiseController.java
@@ -1,11 +1,19 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.model.SmsHomeAdvertise;
+import com.github.cccy0.mall.service.SmsHomeAdvertiseService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 首页轮播广告管理Controller
+ *
  * @author Zhai
  * 2020/11/25 22:14
  */
@@ -14,4 +22,64 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "首页轮播广告管理")
 @RequestMapping("/home/advertise")
 public class SmsHomeAdvertiseController {
+    private SmsHomeAdvertiseService advertiseService;
+
+    @Autowired
+    public void setAdvertiseService(SmsHomeAdvertiseService advertiseService) {
+        this.advertiseService = advertiseService;
+    }
+
+    @ApiOperation("添加广告")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody SmsHomeAdvertise advertise) {
+        int count = advertiseService.create(advertise);
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除广告")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@RequestParam("ids") List<Long> ids) {
+        int count = advertiseService.delete(ids);
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改上下线状态")
+    @RequestMapping(value = "/update/status/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> updateStatus(@PathVariable Long id, Integer status) {
+        int count = advertiseService.updateStatus(id, status);
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("获取广告详情")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<SmsHomeAdvertise> getItem(@PathVariable Long id) {
+        SmsHomeAdvertise advertise = advertiseService.getItem(id);
+        return CommonResult.success(advertise);
+    }
+
+    @ApiOperation("修改广告")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long id, @RequestBody SmsHomeAdvertise advertise) {
+        int count = advertiseService.update(id, advertise);
+        if (count > 0)
+            return CommonResult.success(count);
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("分页查询广告")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsHomeAdvertise>> list(@RequestParam(value = "name", required = false) String name,
+                                                           @RequestParam(value = "type", required = false) Integer type,
+                                                           @RequestParam(value = "endTime", required = false) String endTime,
+                                                           @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                           @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsHomeAdvertise> advertiseList = advertiseService.list(name, type, endTime, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(advertiseList));
+    }
 }
