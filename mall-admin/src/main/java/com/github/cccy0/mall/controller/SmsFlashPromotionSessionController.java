5ff24673259adbb6c4e2b7231aa86cce867d6579
cccy <cccy0@foxmail.com>
2020-11-30 22:37:33 +0800

    :new: flashSession controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsFlashPromotionSessionController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsFlashPromotionSessionController.java
@@ -1,13 +1,18 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.dto.SmsFlashPromotionSessionDetail;
+import com.github.cccy0.mall.model.SmsFlashPromotionSession;
 import com.github.cccy0.mall.service.SmsFlashPromotionSessionService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * todo: 限时购场次管理Controller
+ * 限时购场次管理Controller
  *
  * @author Zhai
  * 2020/11/30 0:34
@@ -21,5 +26,66 @@ public class SmsFlashPromotionSessionController {
     @Autowired
     public void setFlashPromotionSessionService(SmsFlashPromotionSessionService flashPromotionSessionService) {
         this.flashPromotionSessionService = flashPromotionSessionService;
+    }
+
+    @ApiOperation("添加场次")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody SmsFlashPromotionSession promotionSession) {
+        int count = flashPromotionSessionService.create(promotionSession);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改场次")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long id, @RequestBody SmsFlashPromotionSession promotionSession) {
+        int count = flashPromotionSessionService.update(id, promotionSession);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改启用状态")
+    @RequestMapping(value = "/update/status/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> updateStatus(@PathVariable Long id, Integer status) {
+        int count = flashPromotionSessionService.updateStatus(id, status);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除场次")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@PathVariable Long id) {
+        int count = flashPromotionSessionService.delete(id);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("获取场次详情")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<SmsFlashPromotionSession> getItem(@PathVariable Long id) {
+        SmsFlashPromotionSession promotionSession = flashPromotionSessionService.getItem(id);
+        return CommonResult.success(promotionSession);
+    }
+
+    @ApiOperation("获取全部场次")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<List<SmsFlashPromotionSession>> list() {
+        List<SmsFlashPromotionSession> promotionSessionList = flashPromotionSessionService.list();
+        return CommonResult.success(promotionSessionList);
+    }
+
+    @ApiOperation("获取全部可选场次及其数量")
+    @RequestMapping(value = "/selectList", method = RequestMethod.GET)
+    public CommonResult<List<SmsFlashPromotionSessionDetail>> selectList(Long flashPromotionId) {
+        List<SmsFlashPromotionSessionDetail> promotionSessionList = flashPromotionSessionService.selectList(flashPromotionId);
+        return CommonResult.success(promotionSessionList);
     }
 }
