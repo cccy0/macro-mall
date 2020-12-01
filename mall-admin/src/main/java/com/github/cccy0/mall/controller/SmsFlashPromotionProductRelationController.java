8edaf3f25d0a1f37894d6b931f25676f68dec234
cccy <cccy0@foxmail.com>
2020-12-01 22:41:20 +0800

    :new: flashProductRelation controller

---
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsFlashPromotionProductRelationController.java
@@ -0,0 +1,78 @@
+package com.github.cccy0.mall.controller;
+
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.dto.SmsFlashPromotionProduct;
+import com.github.cccy0.mall.model.SmsFlashPromotionProductRelation;
+import com.github.cccy0.mall.service.SmsFlashPromotionProductRelationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 限时购和商品关系管理Controller
+ *
+ * @author Zhai
+ * 2020/11/30 22:38
+ */
+@RestController
+@Api(tags = "限时购和商品关系管理")
+@RequestMapping("/flashProductRelation")
+public class SmsFlashPromotionProductRelationController {
+    private SmsFlashPromotionProductRelationService relationService;
+
+    @Autowired
+    public void setRelationService(SmsFlashPromotionProductRelationService relationService) {
+        this.relationService = relationService;
+    }
+
+    @ApiOperation("批量选择商品添加关联")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody List<SmsFlashPromotionProductRelation> relationList) {
+        int count = relationService.create(relationList);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改关联相关信息")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long id, @RequestBody SmsFlashPromotionProductRelation relation) {
+        int count = relationService.update(id, relation);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除关联")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@PathVariable Long id) {
+        int count = relationService.delete(id);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("获取管理商品促销信息")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<SmsFlashPromotionProductRelation> getItem(@PathVariable Long id) {
+        SmsFlashPromotionProductRelation relation = relationService.getItem(id);
+        return CommonResult.success(relation);
+    }
+
+    @ApiOperation("分页查询不同场次关联及商品信息")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsFlashPromotionProduct>> list(@RequestParam(value = "flashPromotionId") Long flashPromotionId,
+                                                                   @RequestParam(value = "flashPromotionSessionId") Long flashPromotionSessionId,
+                                                                   @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                                   @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsFlashPromotionProduct> flashPromotionProductList = relationService.list(flashPromotionId, flashPromotionSessionId, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(flashPromotionProductList));
+    }
+}
