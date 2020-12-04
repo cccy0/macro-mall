87403765f7ff93f7f0954d9c30793f15ff1fa2ad
cccy <cccy0@foxmail.com>
2020-12-05 00:03:59 +0800

    :new: couponHistory controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsCouponHistoryController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsCouponHistoryController.java
@@ -1,8 +1,15 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.model.SmsCouponHistory;
+import com.github.cccy0.mall.service.SmsCouponHistoryService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 优惠券领取记录管理Controller
@@ -14,4 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "优惠券领取记录管理")
 @RequestMapping("/couponHistory")
 public class SmsCouponHistoryController {
+    private SmsCouponHistoryService historyService;
+
+    @Autowired
+    public void setHistoryService(SmsCouponHistoryService historyService) {
+        this.historyService = historyService;
+    }
+
+    @ApiOperation("根据优惠券id，使用状态，订单编号分页获取领取记录")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<CommonPage<SmsCouponHistory>> list(@RequestParam(value = "couponId", required = false) Long couponId,
+                                                           @RequestParam(value = "useStatus", required = false) Integer useStatus,
+                                                           @RequestParam(value = "orderSn", required = false) String orderSn,
+                                                           @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                           @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsCouponHistory> historyList = historyService.list(couponId, useStatus, orderSn, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(historyList));
+    }
 }
