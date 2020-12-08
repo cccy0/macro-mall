7416088a04bcf65eb0dea1a83406fcfa7b8befab
cccy <cccy0@foxmail.com>
2020-12-09 01:21:53 +0800

    :new: coupon controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsCouponController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/SmsCouponController.java
@@ -1,8 +1,16 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.dto.SmsCouponParam;
+import com.github.cccy0.mall.model.SmsCoupon;
+import com.github.cccy0.mall.service.SmsCouponService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 优惠券管理Controller
@@ -14,4 +22,58 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "优惠券管理")
 @RequestMapping("/coupon")
 public class SmsCouponController {
+    private SmsCouponService couponService;
+
+    @Autowired
+    public void setCouponService(SmsCouponService couponService) {
+        this.couponService = couponService;
+    }
+
+    @ApiOperation("添加优惠券")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> add(@RequestBody SmsCouponParam couponParam) {
+        int count = couponService.create(couponParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除优惠券")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@PathVariable Long id) {
+        int count = couponService.delete(id);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改优惠券")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long id, @RequestBody SmsCouponParam couponParam) {
+        int count = couponService.update(id, couponParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("根据优惠券名称和类型分页获取优惠券列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsCoupon>> list(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "type", required = false) Integer type,
+            @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<SmsCoupon> couponList = couponService.list(name, type, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(couponList));
+    }
+
+    @ApiOperation("获取单个优惠券的详细信息")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<SmsCouponParam> getItem(@PathVariable Long id) {
+        SmsCouponParam couponParam = couponService.getItem(id);
+        return CommonResult.success(couponParam);
+    }
 }
