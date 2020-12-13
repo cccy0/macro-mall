01b3e954f352f825f1885763333d6adb48a1cdc3
cccy <cccy0@foxmail.com>
2020-12-13 23:48:18 +0800

    :new: PmsSkuStock controller

--- mall-admin/src/main/java/com/github/cccy0/mall/controller/PmsSkuStockController.java
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/PmsSkuStockController.java
@@ -1,10 +1,14 @@
 package com.github.cccy0.mall.controller;
 
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.model.PmsSkuStock;
 import com.github.cccy0.mall.service.PmsSkuStockService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * sku库存Controller
@@ -21,5 +25,23 @@ public class PmsSkuStockController {
     @Autowired
     public void setSkuStockService(PmsSkuStockService skuStockService) {
         this.skuStockService = skuStockService;
+    }
+
+    @ApiOperation("根据商品编号及编号模糊搜索sku库存")
+    @RequestMapping(value = "/{pid}", method = RequestMethod.GET)
+    public CommonResult<List<PmsSkuStock>> getList(@PathVariable Long pid, @RequestParam(value = "keyword", required = false) String keyword) {
+        List<PmsSkuStock> skuStockList = skuStockService.getList(pid, keyword);
+        return CommonResult.success(skuStockList);
+    }
+
+    @ApiOperation("批量更新库存信息")
+    @RequestMapping(value = "/update/{pid}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long pid, @RequestBody List<PmsSkuStock> skuStockList) {
+        int count = skuStockService.update(pid, skuStockList);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
     }
 }
