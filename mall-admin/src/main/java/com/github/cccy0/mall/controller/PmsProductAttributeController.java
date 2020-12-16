3defce603a25d06748181cc143180c11cff10418
cccy <cccy0@foxmail.com>
2020-12-17 00:23:07 +0800

    :new: PmsProductAttribute controller

---
+++ mall-admin/src/main/java/com/github/cccy0/mall/controller/PmsProductAttributeController.java
@@ -0,0 +1,92 @@
+package com.github.cccy0.mall.controller;
+
+import com.github.cccy0.mall.common.api.CommonPage;
+import com.github.cccy0.mall.common.api.CommonResult;
+import com.github.cccy0.mall.dto.PmsProductAttributeParam;
+import com.github.cccy0.mall.dto.ProductAttrInfo;
+import com.github.cccy0.mall.model.PmsProductAttribute;
+import com.github.cccy0.mall.service.PmsProductAttributeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 商品属性管理Controller
+ *
+ * @author Zhai
+ * 2020/12/17 0:15
+ */
+@RestController
+@Api(tags = "商品属性管理")
+@RequestMapping("/productAttribute")
+public class PmsProductAttributeController {
+    private PmsProductAttributeService productAttributeService;
+
+    @Autowired
+    public void setProductAttributeService(PmsProductAttributeService productAttributeService) {
+        this.productAttributeService = productAttributeService;
+    }
+
+    @ApiOperation("根据分类查询属性列表或参数列表")
+    @ApiImplicitParams({@ApiImplicitParam(name = "type", value = "0表示属性，1表示参数", required = true, paramType = "query", dataType = "integer")})
+    @RequestMapping(value = "/list/{cid}", method = RequestMethod.GET)
+    public CommonResult<CommonPage<PmsProductAttribute>> getList(@PathVariable Long cid,
+                                                                 @RequestParam(value = "type") Integer type,
+                                                                 @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                                 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        List<PmsProductAttribute> productAttributeList = productAttributeService.getList(cid, type, pageSize, pageNum);
+        return CommonResult.success(CommonPage.restPage(productAttributeList));
+    }
+
+    @ApiOperation("添加商品属性信息")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult<Integer> create(@RequestBody PmsProductAttributeParam productAttributeParam) {
+        int count = productAttributeService.create(productAttributeParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("修改商品属性信息")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult<Integer> update(@PathVariable Long id, @RequestBody PmsProductAttributeParam productAttributeParam) {
+        int count = productAttributeService.update(id, productAttributeParam);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("查询单个商品属性")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<PmsProductAttribute> getItem(@PathVariable Long id) {
+        PmsProductAttribute productAttribute = productAttributeService.getItem(id);
+        return CommonResult.success(productAttribute);
+    }
+
+    @ApiOperation("批量删除商品属性")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult<Integer> delete(@RequestParam("ids") List<Long> ids) {
+        int count = productAttributeService.delete(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("根据商品分类的id获取商品属性及属性分类")
+    @RequestMapping(value = "/attrInfo/{productCategoryId}", method = RequestMethod.GET)
+    public CommonResult<List<ProductAttrInfo>> getAttrInfo(@PathVariable Long productCategoryId) {
+        List<ProductAttrInfo> productAttrInfoList = productAttributeService.getProductAttrInfo(productCategoryId);
+        return CommonResult.success(productAttrInfoList);
+    }
+}
